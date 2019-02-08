20833e85f5df15561473de1095a1c3fd6a67546b
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-08 08:24:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,6 @@
 # Write your code here.
-  word_substituter = {
+
+dictionary = {
     "hello": "hi",
     "to, two, too": "2", 
     "for, four": "4",
@@ -8,20 +9,11 @@
     "at": "@", 
     "and": "&",
   }
+
   
-  def word_substituter(string)
-    "hello" becomes 'hi'
-    "to, two, too" become '2' 
-    "for, four" become '4'
-    'be' becomes 'b'
-    'you' becomes 'u'
-    "at" becomes "@" 
-    "and" becomes "&"
-  end
+def word_substituter(string)
+  dictionary.each do |key, value|
+    
+  end 
+end
   
-  # Question 1
-  describe '#word_substituter' do
-    it 'replaces long words with their expected short form' do
-      expect(word_substituter(tweet_one)).to eq(tweet_one_short)
-    end
-  end
