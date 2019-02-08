b0adae52e735d6d536175898123b6de0d0c9bacf
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-08 08:04:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+  def word_substituter
+    
+  end
+  
+  # Question 1
+  describe '#word_substituter' do
+    it 'replaces long words with their expected short form' do
+      expect(word_substituter(tweet_one)).to eq(tweet_one_short)
+    end
+  end
